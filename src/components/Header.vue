dff42f882a1e1d26b65ea28c6a872f522b6cc289
Irina_Strong_Lady <iogontar@gmail.com>
2023-12-08 15:35:13 +0300

    Fourth commit. Header raw logic

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,6 @@ import { ref, onBeforeMount } from 'vue';
 // Handler for navbar change onscroll
 onBeforeMount(() => {
   window.addEventListener('scroll', handleScroll);
-  window.addEventListener('scroll', verticalSelector);
-  window.addEventListener('scroll', checkCollapseNav);
-  window.addEventListener('resize', changeOnHorizontalResize);
-  window.addEventListener('resize', resizeToSmalToggler);
 });
 
 const handleScroll = () => {
@@ -22,44 +18,11 @@ const firstItem = document.querySelectorAll('.nav-item');
 firstItem.forEach(item => item.addEventListener('click', item.classList.remove('nav-item-onload')));
 };
 
-// Check the bg-dark class not applied on top navbar position in 
-// collapsed state after vertical scrolling and after click and applied in scrolled position
-const checkCollapseNav = () => {
+const addDark = () => {
+  const container = document.querySelector('.container-fluid');
   const toggler = document.querySelector('.navbar-toggler');
-  toggler.classList.contains('collapsed') && window.scrollY == 0 ? checkCollapse.value = false : checkCollapse.value = true;  
+  toggler.addEventListener('click', container.classList?.add('bg-dark'));
 };
-
-const checkCollapse = ref(false);
-
-// Check the bg-dark class not applied on top navbar position in 
-// collapsed state after vertical scrolling from bottom
-const verticalSelector = () => {
-  const vertical = document.querySelector('.vertical-line-logo');
-  const navbar = document.querySelector('.container-fluid');
-  const collapse = document.querySelector('.navbar-collapse');
-  vertical != null && !collapse.classList.contains('show') ? navbar.classList.remove('bg-dark') : navbar.classList.add('bg-dark');  
-};
-
-// Check the 'bg-dark' and 'show' classes not applied on top navbar position in 
-// collapsed state after window resize
-const changeOnHorizontalResize = () => {
-  const collapse = document.querySelector('.navbar-collapse');
-  const vertical = document.querySelector('.vertical-line-logo');
-  const navbar = document.querySelector('.container-fluid');
-  if (collapse.classList.contains('show') && window.innerWidth >= 1000) {
-    collapse.classList.remove('show');
-    }
-  else if (vertical != null) {
-    navbar.classList.remove('bg-dark');
-    }
-  };
-
-// Check the burger toggler after window resize to small
-const resizeToSmalToggler = () => {
-  if (window.innerWidth < 1000) {
-    document.querySelector('.navbar-toggler').setAttribute('aria-expanded', 'false');
-    };
-  };
 
 const props = defineProps({    
     navBarItems: {
@@ -73,27 +36,29 @@ const props = defineProps({
 <template>
   <section>
     <nav class="navbar">
-      <div
-        :class="stickyHeader || checkCollapse ? 'bg-dark' : ''"
+      <div        
+        :class="{'bg-dark': stickyHeader}"
         class="container-fluid navbar-expand-lg navbar-dark"
       >
         <div class="wrapper">
           <div class="navbar-logo">
             <img src="../assets/img/svg/logo-sm.svg" alt="Центр правовой помощи при банкротстве" class="logo">
             <div
-              :class="stickyHeader ? '' : 'vertical-line-logo'"
+              :class="{'vertical-line-logo': !stickyHeader}"
               :style="{'transition': '.5s'}"
             >
             </div>
             <div class="navbar-brand-wrapper">
               <a              
-                :class="stickyHeader ? '' : 'navbar-brand-custom'"
+                :class="{'navbar-brand-custom': !stickyHeader}"
                 :style="{'transition': '.5s', 'text-decoration': 'none'}" 
                 href="#!">Центр правовой помощи при банкротстве
               </a>
             </div>
           </div>
-          <button @click.capture="checkCollapseNav()" class="navbar-toggler" id="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" 
+          @click="addDark"
+          id="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             <div class="navbar-close">
               <span class="navbar-close-line"></span>
@@ -124,10 +89,8 @@ const props = defineProps({
 .container-fluid
   position: fixed
   top: 0
-  padding: 2.5vh 0
+  padding: 1.5vh 0
   z-index: 2
-  @media screen and (max-width: 1000px)
-    padding: 1.5vh 0
 .wrapper
   display: flex
   flex-wrap: nowrap
@@ -229,4 +192,20 @@ const props = defineProps({
     transform: translateY(-50%) rotate(45deg)
   &:last-child
     transform: translateY(-50%) rotate(-45deg) 
+
+
+.header__nav
+  transition: .5s all linear
+  margin: auto 0 auto 0
+  @media screen and (max-width: 767px)
+    position: fixed
+    width: 100%
+    height: 100%
+    background-color: #353D45
+    z-index: 10
+    padding: 75px
+    transform: translateX(100%)
+.header__nav-active
+  transform: translateX(0%)
+  transition: .5s all linear
 </style>
