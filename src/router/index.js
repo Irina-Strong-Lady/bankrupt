4c3e875dbc47e492ec405b5d6f7294717c996feb
Irina_Strong_Lady <iogontar@gmail.com>
2024-07-29 01:28:23 +0300

    Twelve commit (part 4). Adding 404 page routing. Project 100% done.

--- src/router/index.js
+++ src/router/index.js
@@ -7,42 +7,50 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../pages/Home.vue')
-    },    
+    },
     {
       path: '/prices',
       name: 'prices',
       component: () => import('../pages/Prices.vue')
-    },    
+    },
     {
       path: '/services',
       name: 'services',
       component: () => import('../pages/Services.vue')
-    },    
+    },
     {
       path: '/contacts',
       name: 'contacts',
       component: () => import('../pages/Contacts.vue')
-    },    
+    },
     {
       path: '/admins',
       name: 'admins',
       component: () => import('../admin/Admin.vue')
-    },    
+    },
     {
       path: '/tables',
       name: 'tables',
       component: () => import('../admin/pages/Table.vue')
-    },    
+    },
     {
       path: '/profiles',
       name: 'profiles',
       component: () => import('../admin/pages/UserProfile.vue')
-    },   
+    },
     {
       path: '/confidentials',
       name: 'confidentials',
       component: () => import('../pages/ConfidentialLayout.vue')
-    }   
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import('../pages/404.vue')
+    },
+    { path: '/:pathMatch(.*)*',
+      redirect: '/404'
+    }
   ]
 })
 
